4914a3bb2b3ad0f73bab491f1b62e4d38d343162
Mohamed Ibrahim <mosuliman92@gmail.com>
2023-12-30 07:57:02 +0200

    create entites

--- src/main/java/com/mohamed/taskmanagement/entity/Event.java
+++ src/main/java/com/mohamed/taskmanagement/entity/Event.java
@@ -1,4 +1,27 @@
 package com.mohamed.taskmanagement.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "event")
+@Getter @Setter
 public class Event {
+    @Id
+    private String id;
+
+    @Column(name = "summary")
+    private String summary;
+
+    @Column(name = "description")
+    private String description;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "start_id")
+    private EventDateTime start;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "end_id")
+    private EventDateTime end;
 }
--- src/main/java/com/mohamed/taskmanagement/entity/EventDateTime.java
+++ src/main/java/com/mohamed/taskmanagement/entity/EventDateTime.java
@@ -1,4 +1,28 @@
 package com.mohamed.taskmanagement.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "event_date")
+@Getter
+@Setter
 public class EventDateTime {
+
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "date")
+    private LocalDate date;
+
+    @Column(name = "date_time")
+    private LocalDateTime dataTime;
+
+    @Column(name = "time_zone")
+    private String timeZone;
 }
