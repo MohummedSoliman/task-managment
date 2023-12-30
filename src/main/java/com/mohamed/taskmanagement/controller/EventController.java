85a96d79efd1afc933179dca60fa5a6a9424f04e
Mohamed Ibrahim <mosuliman92@gmail.com>
2023-12-30 10:35:25 +0200

    create new endpoint for delete event by id

--- src/main/java/com/mohamed/taskmanagement/controller/EventController.java
+++ src/main/java/com/mohamed/taskmanagement/controller/EventController.java
@@ -5,10 +5,7 @@ import com.mohamed.taskmanagement.service.IEventServcie;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -38,6 +35,12 @@ public class EventController {
     @PostMapping("save")
     public String saveEvent(@ModelAttribute("event") Event event){
         iEventServcie.createEvent(event);
+        return "redirect:/events/event-list";
+    }
+
+    @GetMapping("/delete")
+    public String deleteEvent(@RequestParam("eventId") String id){
+        iEventServcie.deleteEventById(id);
         return "redirect:/events/event-list";
     }
 }
