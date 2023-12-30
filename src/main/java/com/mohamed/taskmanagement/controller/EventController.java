8e9a9fc969a74582d17323852e2eb456d42c2b9a
Mohamed Ibrahim <mosuliman92@gmail.com>
2023-12-30 09:03:59 +0200

    create two endpoint one for get all events and another for show form

--- src/main/java/com/mohamed/taskmanagement/controller/EventController.java
+++ src/main/java/com/mohamed/taskmanagement/controller/EventController.java
@@ -1,15 +1,32 @@
 package com.mohamed.taskmanagement.controller;
 
+import com.mohamed.taskmanagement.entity.Event;
+import com.mohamed.taskmanagement.service.IEventServcie;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
-    @RequestMapping(path = "events")
+@RequestMapping(path = "events")
+@AllArgsConstructor
 public class EventController {
 
+    private IEventServcie iEventServcie;
+
     @GetMapping
-    public String createEvent(){
+    public String createEvent(Model theModel) {
+        List<Event> events = iEventServcie.findAllEvents();
+        theModel.addAttribute("events", events);
+
         return "event-list";
+    }
+
+    @GetMapping("add-event")
+    public String showEventForm(){
+        return "add-event";
     }
 }
