eb53aca240120ab876e674bbb031c56dbade195d
Mohamed Ibrahim <mosuliman92@gmail.com>
2024-01-02 21:29:52 +0200

    create class Event to map when creating

---
+++ src/main/resources/static/Event.js
@@ -0,0 +1,9 @@
+class Event {
+
+    constructor(summary, description,start, end){
+        this.summary = summary;
+        this.description = description;
+        this.start = start;
+        this.end = end;
+    }
+}
